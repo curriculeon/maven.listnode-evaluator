ae069b1264519c450c58cda907cb68534ec51107
git-leon <xleonhunter@gmail.com>
2022-12-02 16:29:44 -0500

    update

--- src/main/java/com/github/curriculeon/ListNodeAdder.java
+++ src/main/java/com/github/curriculeon/ListNodeAdder.java
@@ -12,35 +12,6 @@ package com.github.curriculeon;
  */
 public class ListNodeAdder {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        final ListNodeEvaluator evaluator1 = new ListNodeEvaluator(l1);
-        final ListNodeEvaluator evaluator2 = new ListNodeEvaluator(l2);
-        final Integer sum = evaluator1.getIntegerValue() + evaluator2.getIntegerValue();
-        return new ListNodeEvaluator(sum).getHead();
-    }
-    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
-        String value1 = "";
-        String value2 = "";
-        ListNode prev1 = l1;
-        ListNode prev2 = l2;
-        while(prev1.next != null) {
-            value1+=prev1.val;
-            value2+=prev2.val;
-            prev1 = prev1.next;
-            prev2 = prev2.next;
-        }
-        Integer result = Integer.parseInt(value1) + Integer.parseInt(value2);
-
-        ListNode current = null;
-        ListNode previous = new ListNode();
-        String[] digitsAsString = result.toString().split("");
-        for(int i=1;i<digitsAsString.length;i++) {
-            String previousDigit = digitsAsString[i-1];
-            String currentDigit = digitsAsString[i];
-            current = new ListNode(Integer.parseInt(currentDigit));
-            previous.val = Integer.parseInt(previousDigit);
-            current.next = previous;
-            previous = current;
-        }
-        return current;
+        return null;
     }
 }
--- src/main/java/com/github/curriculeon/ListNodeEvaluator.java
+++ src/main/java/com/github/curriculeon/ListNodeEvaluator.java
@@ -23,54 +23,18 @@ public class ListNodeEvaluator {
 
     public ListNodeEvaluator(Integer... values) {
         this(new ListNode(values[0]));
-        List<Integer> ints = new LinkedList<>();
-        for (int i = 0; i < values.length; i++) {
-            Integer value = values[i];
-            ints.add(value);
-        }
-        Collections.reverse(ints);
-        ListNode currentListNode = null;
-        ListNode previousListNode = head;
-        for (int i = 1; i < ints.size(); i++) {
-            int previousDigit = ints.get(i-1);
-            int currentDigit = ints.get(i);
-            currentListNode = new ListNode(currentDigit);
-            previousListNode.val = previousDigit;
-            previousListNode.next = currentListNode;
-            previousListNode = currentListNode;
-        }
     }
 
     public Integer getIntegerValue() {
-        return Integer.valueOf(getReversedList()
-                .stream()
-                .map(node -> String.valueOf(node.val))
-                .collect(Collectors.toList())
-                .toString()
-                .replaceAll("\\[", "")
-                .replaceAll(",", "")
-                .replaceAll("]", "")
-                .replaceAll(" ", ""));
+        return null;
     }
 
     public List<ListNode> getReversedList() {
-        List<ListNode> list = getList();
-        System.out.println(list);
-        Collections.reverse(list);
-        return list;
+        return null;
     }
 
     public List<ListNode> getList() {
-        List<ListNode> result = new LinkedList<>();
-        ListNode previousNode = head;
-        ListNode currentNode = null;
-        result.add(previousNode);
-        while (previousNode.next != null) {
-            currentNode = previousNode.next;
-            result.add(currentNode);
-            previousNode = currentNode;
-        }
-        return result;
+        return null;
     }
 
     public ListNode getHead() {
